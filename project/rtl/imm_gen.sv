c2ff244d3b92e79a25d62c9b22256b90e8339165
ayesha <139478896+ayeshaanwaar05@users.noreply.github.com>
2025-07-17 15:01:37 +0500

    Update imm_gen.sv

--- project/rtl/imm_gen.sv
+++ project/rtl/imm_gen.sv
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE file for details.
 // SPDX-License-Identifier: Apache-2.0
 //
-// Author: Umer Shahid (@umershahidengr)
+// Author: Navaal Noshi
 // =============================================================================
 // Single-Cycle RISC-V Processor - Immediate Generator (Workshop Skeleton Version)
 // =============================================================================
@@ -15,24 +15,19 @@ module immgen (
 
     always_comb begin
         case (imm_src)
-            // I-type: imm[11:0] = instruction[31:20]
-            3'b000: 
+            3'b000: // I-type
                 imm_ext = {{20{instruction[31]}}, instruction[31:20]};
-            
-            // S-type: imm[11:5] = instruction[31:25], imm[4:0] = instruction[11:7]
-            3'b001:
+
+            3'b001: // S-type
                 imm_ext = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
 
-            // B-type: imm[12|10:5|4:1|11] = instruction[31|30:25|11:8|7], LSB = 0
-            3'b010:
+            3'b010: // B-type
                 imm_ext = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
 
-            // U-type: imm[31:12] = instruction[31:12], LSBs = 12'b0
-            3'b011:
+            3'b011: // U-type
                 imm_ext = {instruction[31:12], 12'b0};
 
-            // J-type: imm[20|10:1|11|19:12] = instruction[31|30:21|20|19:12], LSB = 0
-            3'b100:
+            3'b100: // J-type
                 imm_ext = {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
 
             default:
