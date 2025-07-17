c0e1f2ed45e3995701e422061b2baa8c094e6983
aryamshabbir <aryamshabbir79@gmail.com>
2025-07-17 15:40:58 -0700

    updated imm_gen

--- project/rtl/imm_gen.sv
+++ project/rtl/imm_gen.sv
@@ -17,6 +17,18 @@ module immgen (
         case (imm_src)
             3'b000: // I-type (Example completed)
                 imm_ext = {{20{instruction[31]}}, instruction[31:20]};
+            // S-type: imm = [31:25|11:7]
+            3'b001: imm_ext = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+
+            // B-type: imm = [31|7|30:25|11:8|0]
+            3'b010: imm_ext = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+
+            // U-type: imm = [31:12] << 12
+            3'b011: imm_ext = {instruction[31:12], 12'b0};
+
+            // J-type: imm = [31|19:12|20|30:21|0]
+            3'b100: imm_ext = {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
+
             
             // TODO: Implement remaining immediate types
             // 3'b001: S-type
